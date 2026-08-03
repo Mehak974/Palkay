8ff209f6138ce6440a2af0b3a9c2ab54ecd26a22
Mehak Iqbal <mehakiqbal974@gmail.com>
2026-08-04 03:50:27 +0500

    Redirect passenger stderr to a log file for debugging

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,6 +4,9 @@ import sys
 # Point to your project root
 sys.path.insert(0, os.path.dirname(__file__))
 
+# Redirect error logs to a file we can read in File Manager
+sys.stderr = open(os.path.join(os.path.dirname(__file__), 'passenger_stderr.log'), 'a')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'palkay.settings_production')
 
 from django.core.wsgi import get_wsgi_application
