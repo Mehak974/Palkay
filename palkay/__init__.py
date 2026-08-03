8951914acfa2db7e8a8bea0360c861b8c030241b
Mehak Iqbal <mehakiqbal974@gmail.com>
2026-08-04 01:39:31 +0500

    Use PyMySQL instead of mysqlclient for shared hosting

--- palkay/__init__.py
+++ palkay/__init__.py
@@ -1,3 +1,9 @@
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 try:
